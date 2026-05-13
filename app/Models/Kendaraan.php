49837c6bb8d1b1754e62417dbfebe1260c27eab7
ginaahutapeaa <ginaahutapeaa@gmail.com>
2026-05-13 13:24:58 +0700

    membuat konfigurasi model kendaraan

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    //
+    protected $fillable = [
+        'plat_nomor',
+        'nama_pemilik',
+        'merk_kendaraan',
+        'keluhan'
+    ];
 }
